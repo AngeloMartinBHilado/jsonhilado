8fb8b3277633a756f8389bf8fadd8f40f92e89e4
AngeloMartinBHilado <56312511+AngeloMartinBHilado@users.noreply.github.com>
2020-02-13 17:24:39 +0800

    Update json.php

--- json.php
+++ json.php
@@ -3,7 +3,7 @@ include 'config.php';
 include 'class.bag.php';
 header('Content-Type: application/json');
 $bag = new bag();
-$list=$bag->get_bag();
+$list=$bag->get_store();
 echo "{\"bag\":";
 echo json_encode($list);
 echo "}";
